eb20a3edc78ab64c5f02663d0d03c1e8dcfa5ea1
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-31 12:16:53 +0400

    added function for read url from txt file

--- search_url.py
+++ search_url.py
@@ -1,6 +1,9 @@
 import re
 
 
-urls_text = open('urls.txt').read()
-r = re.compile(r"(http://[^ ]+)")
-urls = r.sub(r'<a href="\1">\1</a>', urls_text).split('\n')
+def get_url_from_txt():
+    urls_text = open('urls.txt').read()
+    r = re.compile(r"(http://[^ ]+)")
+    urls = r.sub(r'<a href="\1">\1</a>', urls_text).split('\n')
+    for url in urls:
+        yield url
