9910e188b75f9b4956b1f63b4ffc224ac2a37b16
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-31 21:41:06 +0400

    added conftest file

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--my_test_dump", action="store", default=None,
+        help="Print test items in my custom format")
+
+
+def pytest_collection_finish(session):
+    if session.config.option.my_test_dump is not None:
+        for item in session.items:
+            print('{}::{}'.format(item.fspath, item.name))
+        pytest.exit('Done!')
