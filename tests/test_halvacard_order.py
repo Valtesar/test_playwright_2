78098ac76618a1e6662ea7cec5059eb550555481
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-01 12:36:37 +0400

    fix func name

--- tests/test_halvacard_order.py
+++ tests/test_halvacard_order.py
@@ -2,7 +2,7 @@ import pytest
 from playwright.sync_api import Page
 from pages.sovcombank.cards_page import CardsPage
 import logging
-
+from time import sleep
 
 logger = logging.getLogger("tests")
 
@@ -16,11 +16,10 @@ def main_bank_page(page: Page):
 
 
 @pytest.mark.only_browser("chromium")
-def check_validation_message_is_visible(main_bank_page):
+def test_validation_message_is_visible(main_bank_page):
     bank_page = main_bank_page
     bank_page.go_to_section('Карты')
     bank_page.order_halva_card()
     bank_page.edit_check_box_order()
     assert bank_page.get_check_box_error()
     assert bank_page.get_fields_and_warnings()
-
