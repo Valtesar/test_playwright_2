11ce5a00dc4dc60e1d29c2eadda188bf489169e5
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-02 02:33:09 +0400

    update doc string

--- tests/test_check_result_of_search.py
+++ tests/test_check_result_of_search.py
@@ -40,6 +40,10 @@ def main_search_page(page: Page, get_link_from_file):
 
 @pytest.mark.only_browser("chromium")
 def test_check_result_of_search(main_search_page):
+    """Тест выполняет запрос в поисковой строке используя необходимое ключевое слово.
+        Затем находит в результатах выдачи необходимый сайт и переходит на него.
+        Считывает кол-во найденных результатов и выводит их в консоль"""
+
     search_page = main_search_page
     search_page.search_text('Совкомбанк')
     search_page.get_stats()
