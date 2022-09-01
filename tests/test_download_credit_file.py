177f18af8db605f95524fa70a6bc58a58fbd4e55
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-02 02:33:12 +0400

    update doc string

--- tests/test_download_credit_file.py
+++ tests/test_download_credit_file.py
@@ -17,6 +17,9 @@ def main_bank_page(page: Page):
 
 @pytest.mark.only_browser("chromium")
 def test_download_credit_file(main_bank_page):
+    """Тест с главной страницы сайта переходит в секцию кредиты. Затем скачивает файл путем клика по нему.
+        Дожидается загрузки объекта. Затем проверяет является ли объект файлом и удаляет данный файл"""
+
     bank_page = main_bank_page
     bank_page.go_to_section('Кредиты')
     bank_page.download_file()
