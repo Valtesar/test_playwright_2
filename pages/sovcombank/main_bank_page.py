8a8b7dbf8de06439e36f8d46a6bd79cadff69fca
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-01 11:21:26 +0400

    added waiter for selector

--- pages/sovcombank/main_bank_page.py
+++ pages/sovcombank/main_bank_page.py
@@ -6,7 +6,7 @@ class MainBankPage(WebPage):
     def __init__(self, base_url, page: Page):
         super().__init__(page)
         self.base_url = base_url
-        self.sections = ['Карты', 'Кредиты']
+        self.sections = ['Карты']
 
     def open(self):
         self.page.goto(self.base_url, wait_until="load")
@@ -15,4 +15,5 @@ class MainBankPage(WebPage):
     def go_to_section(self, search_section):
         for page_section in self.sections:
             if page_section == search_section:
-                self.page.click(f'//a[@class="dark:link-text-tertiary link  py-5 link-b-bg" and text()="{page_section}"]')
+                self.page.click(f'//a[@class="dark:link-text-tertiary link  py-5 " and text()="{page_section}"]')
+                self.page.wait_for_selector('//*[@id="__next"]')
