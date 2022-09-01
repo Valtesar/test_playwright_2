ac5f744706902dd466a2490548d13fafa063d9dd
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-01 22:17:49 +0400

    added functions for download file, check file, delete file added doc strings

--- pages/sovcombank/credits_page.py
+++ pages/sovcombank/credits_page.py
@@ -1,15 +1,30 @@
-from playwright.sync_api import Page
 from pages.sovcombank.main_bank_page import MainBankPage
+from pathlib import Path
 
 
 class CreditsPage(MainBankPage):
     def download_file(self):
-        # Click to download file
-        # Check that files downloaded -> 1, no -> 0
-        pass
+        """Функция кликает на файл находящийся на странице, дожидается его скачивания.
+            Для наглядности выводит путь файла на компьютере."""
+
+        with self.page.expect_download() as download_info:
+            self.page.locator('(//div[@class="font-medium group-hover:text-brand-blue"])[2]').click()
+        self.download = download_info.value
+        print(self.download.path())
 
     def check_downloaded_files(self):
-        # check type of files
-        # delete files
-        # -> return true if deleted -> else false
-        pass
+        """Функция проверяет тип скаченного объекта.
+            Если объект является файлом возвращает True, иначе False"""
+
+        path = Path(self.download.path())
+        return True if path.is_file() else False
+
+    def delete_downloaded_files(self):
+        """Функция удаляет скаченный файл. Если файл удален возвращает True, иначе False"""
+
+        path = Path(self.download.path())
+        try:
+            path.unlink()
+            return True
+        except FileNotFoundError:
+            return False
