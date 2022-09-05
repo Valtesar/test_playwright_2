fe3a19de86d059cc88b633105b6c7ae3cd1ff0b8
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-09-05 10:26:29 +0400

    transfer fixtures from test for test search string

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
 import pytest
+from distutils import dir_util
+import os
+from playwright.sync_api import Page
+from search_url import get_url_from_txt
+from pages.search_engine.main_search_page import SearchPage
 
 
 def pytest_addoption(parser):
@@ -11,3 +16,33 @@ def pytest_collection_finish(session):
         for item in session.items:
             print('{}::{}'.format(item.fspath, item.name))
         pytest.exit('Done!')
+
+
+@pytest.mark.onlytest("test_check_result_of_search")
+@pytest.fixture()
+def data_dir(tmpdir, request):
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmpdir))
+
+    return tmpdir
+
+
+@pytest.mark.onlytest("test_check_result_of_search")
+@pytest.fixture()
+def get_link_from_file(data_dir):
+    file = data_dir.join('urls.txt')
+    with open(file) as text_file:
+        link = get_url_from_txt(text_file.read())
+    return link
+
+
+@pytest.mark.onlytest("test_check_result_of_search")
+@pytest.fixture()
+def main_search_page(page: Page, get_link_from_file):
+    search_page = SearchPage(get_link_from_file, page)
+    search_page.delete_cookies()
+    search_page.open()
+    return search_page
